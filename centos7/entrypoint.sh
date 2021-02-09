e03c57254ff247604d22d6a6d5def06c024bf95a
Shakir Ali <shakir.ali@get10up.com>
2021-02-09 12:24:27 +0500

    added php session variables for centos

--- centos7/entrypoint.sh
+++ centos7/entrypoint.sh
@@ -7,6 +7,19 @@ port ${MAILER_PORT:-1025}
 auto_from on
 EOF
 
+if [ ! -z "${SESSION_HANDLER}" ]; then
+cat >> /etc/php-fpm.d/www.conf <<EOF
+php_value[session.save_handler] = ${SESSION_HANDLER}
+php_value[session.save_path]    = '${SESSION_PATH}'
+EOF
+else
+cat >> /etc/php-fpm.d/www.conf <<EOF
+php_value[session.save_handler] = files
+php_value[session.save_path]    = '/var/lib/php/sessions'
+EOF
+fi
+
+
 if [ ! -z "${NR_LICENSE_KEY}" ]; then
 cat > /etc/php.d/newrelic.ini <<EOF
 newrelic.enabled = true
