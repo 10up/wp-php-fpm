f89832c94317e592954d622e15d69957e01f004a
James Golovich <james@gnuinter.net>
2022-08-24 16:58:15 -0700

    Use correct session directory for centos

--- centos8/entrypoint.sh
+++ centos8/entrypoint.sh
@@ -9,7 +9,7 @@ EOF
 
 cat >> /etc/php-fpm.d/www.conf <<EOF
 php_value[session.save_handler] = ${SESSION_HANDLER:-files}
-php_value[session.save_path]    = '${SESSION_PATH:-/var/lib/php/sessions}'
+php_value[session.save_path]    = '${SESSION_PATH:-/var/lib/php/session}'
 EOF
 
 if [ ! -z "${NR_LICENSE_KEY}" ]; then
